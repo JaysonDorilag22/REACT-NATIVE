526f742171cd1d786767c8a0d2e82e3410d45fd2
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-14 19:31:12 +0800

    user full backend CRUD

--- server/app.js
+++ server/app.js
@@ -26,11 +26,11 @@ app.get("/", (req, res, next) => {
 });
 
 // Importing Routers here
-// import user from "./routes/user.js";
+import user from "./routes/user.js";
 // import product from "./routes/product.js";
 // import order from "./routes/order.js";
 
-// app.use("/api/v1/user", user);
+app.use("/api/v1/user", user);
 // app.use("/api/v1/product", product);
 // app.use("/api/v1/order", order);
 
