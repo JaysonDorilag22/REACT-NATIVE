c782bcc59757c286f29dbdbb3ad06ca2f14f0bf5
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-15 17:35:48 +0800

    backend rn

--- server/app.js
+++ server/app.js
@@ -27,12 +27,12 @@ app.get("/", (req, res, next) => {
 
 // Importing Routers here
 import user from "./routes/user.js";
-// import product from "./routes/product.js";
-// import order from "./routes/order.js";
+import product from "./routes/product.js";
+import order from "./routes/order.js";
 
 app.use("/api/v1/user", user);
-// app.use("/api/v1/product", product);
-// app.use("/api/v1/order", order);
+app.use("/api/v1/product", product);
+app.use("/api/v1/order", order);
 
 // Using Error Middleware
 app.use(errorMiddleware);
